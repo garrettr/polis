ce8fa953454cd8f28f91b25a891fb4c029c213d5
Garrett Robinson <garrett.f.robinson@gmail.com>
2012-07-01 22:25:23 -0700

    Fixed app.run

--- run.py
+++ run.py
@@ -65,7 +65,6 @@ def address_form():
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-    #app.run(debug=True)
-    app.run()
+    #port = int(os.environ.get('PORT', 5000))
+    #app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
