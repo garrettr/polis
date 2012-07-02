c67ef70f81430c60753d5ceba9df5a6e761afac0
Garrett Robinson <garrett.f.robinson@gmail.com>
2012-07-01 21:17:19 -0700

    Update app debug setting for Heroku deploy

--- run.py
+++ run.py
@@ -62,6 +62,4 @@ def address_form():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # demo on local network
-    #app.run(debug=False, host='0.0.0.0')
+    app.run()
